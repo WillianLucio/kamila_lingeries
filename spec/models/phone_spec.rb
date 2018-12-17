2606b011e41a741ca844cbef2130f2af2e2e29ad
willian <willian.l.lucio@gmail.com>
2018-12-17 11:01:34 -0200

    Implemented phone tests.

--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Phone, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Testing associations' do
+    it 'Belongs to profile' do
+      assc = described_class.reflect_on_association(:profile)
+      expect(assc.macro).to eq :belongs_to
+    end
+  end
+
+  context 'Testing Validations' do
+    it 'Valid attributes' do
+      phone = build(:phone)
+      expect(phone).to be_valid
+    end
+
+    it 'Is invalid without profile' do
+      phone = build(:phone, profile: nil)
+      expect(phone).to_not be_valid
+    end
+  end
 end
