86f71907db84d7d78979d72cd119bd5a91c364e4
willian <willian.l.lucio@gmail.com>
2018-12-17 17:13:50 -0200

    Reorganized css and js.

---
+++ app/controllers/backoffice/categories_controller.rb
@@ -0,0 +1,21 @@
+module Backoffice
+  class CategoriesController < BackofficeController
+    def index
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   namespace :backoffice do
+    get 'categories/index'
+    get 'categories/new'
+    get 'categories/create'
+    get 'categories/edit'
+    get 'categories/update'
+    get 'categories/destroy'
+  end
+  namespace :backoffice do
     resources :dashboards, only: :index
   end
-  devise_for :users, skip: :registration
+  devise_for :users
   root 'home#index'
 end
