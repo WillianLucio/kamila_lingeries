f8abd0992b47ed16345249eed62fe59955a552c4
willian <willian.l.lucio@gmail.com>
2018-12-17 10:15:16 -0200

    Implemented model profile tests.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,10 @@
 class Profile < ApplicationRecord
   belongs_to :user
+  has_one :address
+  has_many :phones
+
+  validates :role, presence: true
+  validates :role, numericality: {  only_integer: true,
+                                    greater_than: 0,
+                                    less_than: 3 }
 end
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Testing associations' do
+    it 'Belongs to user' do
+      assc = described_class.reflect_on_association(:user)
+      expect(assc.macro).to eq :belongs_to
+    end
+
+    it 'Has one address' do
+      assc = described_class.reflect_on_association(:address)
+      expect(assc.macro).to eq :has_one
+    end
+
+    it 'Has many phones' do
+      assc = described_class.reflect_on_association(:phones)
+      expect(assc.macro).to eq :has_many
+    end
+  end
+
+  context 'Testing validations' do
+    it 'Valid attributes' do
+      profile = build(:profile)
+      expect(profile).to be_valid
+    end
+
+    context 'Invalid attributes' do
+      it 'Is invalid without user' do
+        profile = build(:profile, user: nil)
+        expect(profile).to_not be_valid
+      end
+
+      it 'Is invalid without role' do
+        profile = build(:profile, role: nil)
+        expect(profile).to_not be_valid
+      end
+
+      it 'Is invalid with role less than 1' do
+        profile = build(:profile, role: 0)
+        expect(profile).to_not be_valid
+      end
+
+      it 'Is invalid with role greater than 2' do
+        profile = build(:profile, role: 3)
+        expect(profile).to_not be_valid
+      end
+    end
+  end
 end
