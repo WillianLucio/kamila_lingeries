e0f2c7ae34b1a00f2e75f2f894d3ae25496e11e6
willian <willian.l.lucio@gmail.com>
2018-12-17 10:51:34 -0200

    Refactoring user tests and configure rspec format.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,23 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'User is valid with email and password' do
-    user = build(:user)
-    expect(user).to be_valid
+  context 'Testing associations' do
+    it 'Has one profile' do
+      assc = described_class.reflect_on_association(:profile)
+      expect(assc.macro).to eq :has_one
+    end
   end
 
-  it 'User is not valid without email' do
-    user = build(:user, email: nil)
-    expect(user).to_not be_valid
-  end
+  context 'Testing validations' do
+    it 'Valid attributes' do
+      user = build(:user)
+      expect(user).to be_valid
+    end
 
-  it 'User is not valid without password' do
-    user = build(:user, password: nil)
-    expect(user).to_not be_valid
-  end
+    context 'Invalid attributes' do
+      it 'Is invalid without email' do
+        user = build(:user, email: nil)
+        expect(user).to_not be_valid
+      end
 
-  it 'User has one profile' do
-    assc = described_class.reflect_on_association(:profile)
-    expect(assc.macro).to eq :has_one
+      it 'Is invalid without password' do
+        user = build(:user, password: nil)
+        expect(user).to_not be_valid
+      end
+    end
   end
 end
