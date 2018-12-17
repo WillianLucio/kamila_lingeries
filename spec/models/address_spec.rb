ff3da1ad112cfcd01e56ccc0e1874bd1a5057ad2
willian <willian.l.lucio@gmail.com>
2018-12-17 11:16:00 -0200

    Implemented address tests.

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Testing associations' do
+    it 'Belongs to profile' do
+      assc = described_class.reflect_on_association(:profile)
+      expect(assc.macro).to eq :belongs_to
+    end
+  end
+
+  context 'Testing Validations' do
+    it 'Valid attributes' do
+      address = build(:address)
+      expect(address).to be_valid
+    end
+
+    it 'Is invalid without profile' do
+      address = build(:address, profile: nil)
+      expect(address).to_not be_valid
+    end
+  end
 end
