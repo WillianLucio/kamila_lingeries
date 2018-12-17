af29f999aff06ffaf409d6818e422e200c84cf9b
willian <willian.l.lucio@gmail.com>
2018-12-17 09:25:28 -0200

    Implemented model user tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'User is valid with email and password' do
+    user = build(:user)
+    expect(user).to be_valid
+  end
+
+  it 'User is not valid without email' do
+    user = build(:user, email: nil)
+    expect(user).to_not be_valid
+  end
+
+  it 'User is not valid without password' do
+    user = build(:user, password: nil)
+    expect(user).to_not be_valid
+  end
+
+  it 'User has one profile' do
+    assc = described_class.reflect_on_association(:profile)
+    expect(assc.macro).to eq :has_one
+  end
 end
