4d0e9c3c22286ef3bf12859ae5adac1036a38864
willian <willian.l.lucio@gmail.com>
2018-12-24 08:54:20 -0200

    Created migrations for category and subcategory.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_14_121755) do
+ActiveRecord::Schema.define(version: 2018_12_21_111717) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "profile_id"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2018_12_14_121755) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["profile_id"], name: "index_addresses_on_profile_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "phones", force: :cascade do |t|
@@ -44,6 +50,14 @@ ActiveRecord::Schema.define(version: 2018_12_14_121755) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "subcategories", force: :cascade do |t|
+    t.string "name"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_subcategories_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
